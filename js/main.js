5d5361b68f10ac1ad031abac8349e4a6d9e89038
Yosef Waysman <yosefwaysman@gmail.com>
2015-02-07 20:07:55 +0200

    Trying out new TOC plugin

--- js/main.js
+++ js/main.js
@@ -1,5 +1,26 @@
 $(document).ready(function() {
-  $('#main_content').toc({
-    showAlways: true
+  
+  // Generate table of contents
+  $('#toc').toc({
+    listType: 'ul',
+    headers: 'h2, h3, h4',
+    showSpeed: 0
   });
+  
+  // Remove unnesseccary <i>
+  $('i').remove();
+  
+  // Add scrollspy
+  $('body').scrollspy({
+    target: '#toc'
+  });
+  
+  // Turn TOC into Bootstrap affix nav
+  $('#toc ul').addClass('nav nav-pills nav-stacked');
+  $('#toc').affix({
+    offset: {
+      top: 200
+    }
+  });
+  
 });
