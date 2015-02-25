0751d606119dfe96c1325409ecc92d8b57231152
Yosef Waysman <yosefwaysman@gmail.com>
2015-02-25 15:16:26 +0200

    Implemented bootstrap-switch

--- js/main.js
+++ js/main.js
@@ -19,11 +19,29 @@ $(document).ready(function() {
   $('#toc ul').addClass('nav nav-pills nav-stacked');
   $('#toc').affix({
     offset: {
-      top: 200
+      top: 1200
     }
   });
 
   // Add ".lead" to abstract
-  $('main p:first-child').addClass('lead');
+  $('main > p:first-child').addClass('lead');
+
+  // Make all images responsive
+  $('img').addClass('img-responsive');
+
+  // Display mode
+
+  var textElements = "p, h2, h3, h4, ul, ol"; // elements to hide in picture-mode
+
+  $("[name='display-mode']").bootstrapSwitch();
+
+  $('input[name="display-mode"]').on('switchChange.bootstrapSwitch', function(event, state) {
+    if (state == true) {
+      $(textElements).hide();
+    }
+    else {
+      $(textElements).show();
+    }
+  });
 
 });
