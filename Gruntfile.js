ce9fcd0f481e76f9bf05e3b0a7b3a24470ad9096
Yosef Waysman <yosefwaysman@gmail.com>
2015-06-02 18:38:45 +0300

    Setup Grunt

---
+++ Gruntfile.js
@@ -0,0 +1,50 @@
+module.exports = function(grunt) {
+  
+  grunt.initConfig({
+    less: {
+      development: {
+        options: {
+          paths: ['less'],
+          plugins: [
+            new (require('less-plugin-autoprefix'))({browsers : [ 'last 2 versions' ]}),
+            new (require('less-plugin-clean-css'))()
+          ]
+        },
+        files: {
+          'main.css': 'less/main.less'
+        }
+      }
+    },
+    
+    browserSync: {
+      bsFiles: {
+        src : ['_site/**/*.*']
+      },
+      options: {
+        proxy : "localhost:4000",
+        watchTask : true,
+        baseDir: '_site/'
+      }
+    },
+    
+    watch: {
+      styles: {
+        files: 'less/**/*.less',
+        tasks: ['less']
+      },
+      images : {
+          files : ['_site/**/*.{png,jpg,gif}'],
+          tasks : ['imagemin']
+      },
+    }
+  });
+  
+  
+  // Load grunt plugins.
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-browser-sync');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
+  
+  grunt.registerTask('default', ['browserSync', 'watch']);
+};
